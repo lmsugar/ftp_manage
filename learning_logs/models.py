8bc4478aebaa3324acce059a73d3db800287c9db
Li Mingshu <547105745@qq.com>
2017-02-16 14:46:30 +0800

    first commit

---
+++ learning_logs/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Topic(models.Model):
+	text = models.CharField(max_length = 200)
+	
+	date_added = models.DateTimeField(auto_now_add = True)
+	owner = models.ForeignKey(User)
+	
+	def __str__(self):
+		return self.text
+
+
+
+# Create your models here.
+class Entry(models.Model):
+	
+	topic = models.ForeignKey(Topic)
+	text = models.TextField()
+	date_added = models.DateTimeField(auto_now_add = True)
+	owner = models.ForeignKey(User)
+	
+	class Meta:
+		verbose_name_plural = 'entries'
+	
+	def __str__(self):
+
+		return self.text[:50] + '...'
